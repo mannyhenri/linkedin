03a7cd9876018d53665ea3ef035f5af208aace53
Emmanuel Henri <manny@algofields.com>
2020-02-03 11:38:41 +0100

    Initial Commit

---
+++ linkedin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  linkedin
+//
+//  Created by producer on 2/3/20.
+//  Copyright © 2020 Algofields. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
